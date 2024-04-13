d88c50de86c03fae653176f22ada9d67dbdd2d4c
ceejaycodes <dreelceejay79@yahoo.com>
2024-04-13 09:50:50 +0100

    initial commit

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,18 +1,74 @@
-import { describe, it } from 'vitest';
-import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import App from './App';
 
 describe("App", () => {
   it("should allow the user to enter their wish", async () => {
     render(<App />);
+    expect(screen.findByPlaceholderText("enter your wish")).toBeTruthy()
+    expect((await screen.findByPlaceholderText("enter your wish")).textContent).toBe("")
+    
   });
 
   it("should enable button after a wish is entered", async () => {
     render(<App />);
+    const input = document.querySelector(
+      'input'
+    ) as HTMLInputElement;
+
+    const button = document.querySelector(
+      'button'
+    ) as HTMLButtonElement
+    
+    fireEvent.change(input, {
+      target: {
+        value: '*'
+      }
+    })
+
+    expect(button.disabled).toBeFalsy()
+
+
+      
   });
 
   it("should replace the wish entry area with the wish after submission", async () => {    
     render(<App />);
+
+    const input = document.querySelector(
+      'input'
+    ) as HTMLInputElement
+
+    const button = document.querySelector(
+      'button'
+    ) as HTMLButtonElement
+
+  
+
+    fireEvent.change(input, {
+      target: {
+        value: '*'
+      }
+    })
+    fireEvent.click(button)
+    
+
+    screen.debug()
+
+
+    const wish = document.querySelector(
+      'p'
+    ) as HTMLElement
+    // screen.getByRole(" ")
+
+    expect(input).not.toBeNull()
+    expect(button).not.toBeNull()
+
+    expect(wish?.innerHTML).toBe('*')
+
+
+
+
   });
 })
